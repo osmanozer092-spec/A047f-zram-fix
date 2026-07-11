41e4434ee401f8ffe4a6de7981c98cb306f056bf
osmanozer092-spec <osmanozer092@gmail.com>
2026-07-12 00:45:45 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,11 @@ export ARCH=arm64
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 
-make ARCH=arm64 exynos850-a04sxx_defconfig
-make ARCH=arm64 -j16
+# Çıktı dizinini oluştur
+mkdir -p out
+
+# Config ayarlarını out klasörüne yap (sed değişikliğini buraya uygula)
+make ARCH=arm64 O=out exynos850-a04sxx_defconfig
+
+# Derlemeyi out dizinine yap
+make ARCH=arm64 O=out -j16
